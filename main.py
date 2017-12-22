94acd25ae30e65cedf4395769aa904eba0a29728
Steven Pichardo <stevepichardo@outlook.com>
2017-12-22 06:10:40 -0500

    got URLs working

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
+@app.route("/signup")
 def index():
     return render_template("form.html")
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    username = request.form['username']
+    return redirect("/welcome")
+
+@app.route("/welcome")
+def welcome():
+    return "hello"
+
+
 
 app.run()

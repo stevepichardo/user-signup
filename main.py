bcbff76e86e15c0e8d20a1171ec88b47183b41b8
Steven Pichardo <stevepichardo@outlook.com>
2017-12-22 06:22:57 -0500

    wrote code to verify password

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ def index():
 @app.route("/signup", methods=['POST'])
 def signup():
     username = request.form['username']
-    return redirect("/welcome")
+    password = request.form['password']
+    verifypassword = request.form['verifypassword']
+    if password != verifypassword:
+        return "Try Again"
+    return redirect("/welcome?username={0}".format(username))
 
 @app.route("/welcome")
 def welcome():
-    return "hello"
+    username = request.args.get('username')
+    return render_template("welcome.html",username=username)
 
 
 
